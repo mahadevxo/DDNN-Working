e8e51f33d9511b47bef2d49c4c22780f600244b5
mahadevxo <mahadevsunilkumar03@gmail.com>
2024-12-01 17:25:22 +0530

    fixed load_model.py

--- load_model.py
+++ load_model.py
@@ -1,10 +1,10 @@
-from torch import device
+from torch import device as tdevice
 from torch import load
-from torch import cuda
+from torch import cuda as tcuda
 from models import MVCNN
 
 def load_model(weights_path):
-    device = device("cuda" if cuda.is_available() else "cpu")
+    device = tdevice("cuda" if tcuda.is_available() else "cpu")
     
     svcnn_model = MVCNN.SVCNN('svcnn').to(device)
     svcnn_weights = load(weights_path)
