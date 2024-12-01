a7114ed8ae152e05e7f16f62be45b35e736eb16a
mahadevxo <mahadevsunilkumar03@gmail.com>
2024-12-01 23:22:42 +0530

    debug

--- load_model.py
+++ load_model.py
@@ -4,7 +4,7 @@ def load_model(weights_path):
     from torch import cuda as tcuda
     from torch import device as tdevice
     device = tdevice("cuda" if tcuda.is_available() else "cpu")
-    sys.modules.pop('cuda')
+    sys.modules.pop('tcuda')
     del tcuda
     sys.modules.pop('device')
     del tdevice
