ed68fde29ad95a117d1cf36f1d41fc21f0f950a4
mahadevxo <mahadevsunilkumar03@gmail.com>
2024-12-07 06:53:47 +0530

    added multi threading

--- jetson_socket/main_client_multi_thread.py
+++ jetson_socket/main_client_multi_thread.py
@@ -5,6 +5,8 @@ from JetsonClient import JetsonClient
 from load_model import load_model
 from preprocess_images import preprocess_images
 from torch import no_grad
+import base64
+import os
 
 data_queue = queue.Queue()
 
@@ -30,9 +32,8 @@ def worker(client):
         
 def main():    
     address =['100.86.4.56', 4044]
-    
-    
     client = JetsonClient(address[0], int(address[1]))
+    
     thread = Thread(target=worker, args=(client,))
     thread.start()
     
@@ -49,7 +50,6 @@ def main():
     
     print("Model Loaded")
     
-    
     print("Starting image preprocessing")
     images = preprocess_images()
     print("Image Preprocess Done.")
@@ -65,26 +65,34 @@ def main():
                 image = image.to("cuda")
                 
                 start_time = time.time()
-                _ = svcnn(image.unsqueeze(0))
+                pred = svcnn(image.unsqueeze(0))
                 end_time = time.time()
                 
-                # time_process = end_time - start_time
+                time_process = end_time - start_time
+                
+                # convert pred to bytes
+                pred = pred.cpu().numpy().tobytes()
+                pred = base64.b64encode(pred).decode('utf-8')
                 
                 send_time = time.time()
                 
-                # data = f"{send_time},{time_process},{image_count}"
-                data = send_time()
-                image_count += 1                
-                print(f"send time: {send_time}")
+                data = f"{send_time},{time_process},{image_count},{pred}"
+                # print(data)
+                image_count += 1
                 
                 data_queue.put(data)
+                
+                #print(f"Send Time: {send_time}")
+        print("Inference Done")
+        data_queue.put(None)
+        thread.join()
+        exit()
+    
     except Exception as e:
         print(f"Error during inference: {e}")
-    finally:
+        data_queue.put(None)
         thread.join()
-    
-    print("Inference Done")
-    exit()
+        exit()
 
 if __name__ == "__main__":
     main()
