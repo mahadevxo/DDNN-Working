bc2df9199ed898087e3b7941233e5b8ca497e2e7
mahadevxo <mahadevxunilkumar03@gmail.com>
2025-01-30 17:01:34 +0530

    edited

--- pruning_optimize/com_vs_sparsity.py
+++ pruning_optimize/com_vs_sparsity.py
@@ -2,23 +2,31 @@ import torch
 import torch.nn as nn
 import torch.nn.utils.prune as prune
 from torchvision.models import vgg11
-import numpy
 import time
 import matplotlib.pyplot as plt
+import os
 
-computation_time_results = []
-sparsityies = []
-def prune_model(model, sparsity):
-    prunable_layers = [
-        (name, module) for name, module in model.named_modules()
-        if isinstance(module, (nn.Conv2d))
-    ]
+computation_time_results = {}
+
+
+def get_pruned_models():
+    path = './vgg11_pruned/'
+    prune_models = []
+    for file in os.listdir(path):
+        prune_models.append(path+file)
+    return prune_models
+
+# def prune_model(model, sparsity):
+#     prunable_layers = [
+#         (name, module) for name, module in model.named_modules()
+#         if isinstance(module, (nn.Conv2d))
+#     ]
     
-    for _, module in prunable_layers:
-        prune.l1_unstructured(module, name="weight", amount=sparsity)
-        prune.remove(module, name="weight")
+#     for _, module in prunable_layers:
+#         prune.l1_unstructured(module, name="weight", amount=sparsity)
+#         prune.remove(module, name="weight")
     
-    return model
+#     return model
 
 def compute_time(model):
     model = model.cuda()
@@ -34,25 +42,41 @@ def compute_time(model):
 
 print("Imported all libaries")
 
-for sparsity in numpy.arange(0.0, 1.01, 0.01):
-    print("Starting with")
-    print(f"Sparsity: {sparsity}")
-    model = vgg11(pretrained=True)
-    print("Model loaded")
-    model = prune_model(model, sparsity)
-    print("Model pruned")
+pruned_models = get_pruned_models()
+for model_path in pruned_models:
+    model = vgg11(pretrained=False)
+    model.load_state_dict(torch.load(model_path))
     time_taken = compute_time(model)
     print(f"Time taken: {time_taken}")
-    computation_time_results.append(time_taken)
-    sparsityies.append(sparsity)
+    #vgg11_0.7.pth
+    sparsity = model_path.split('_')[1].split('.')[0:2]
+    sparsity = float(sparsity[0] + '.' + sparsity[1])
+    computation_time_results[sparsity] = time_taken
+    
+computation_time_results = dict(sorted(computation_time_results.items()))
+
+# for sparsity in numpy.arange(0.0, 1.01, 0.01):
+#     print("Starting with")
+#     print(f"Sparsity: {sparsity}")
+#     model = vgg11(pretrained=True)
+#     print("Model loaded")
+#     model = prune_model(model, sparsity)
+#     print("Model pruned")
+#     time_taken = compute_time(model)
+#     print(f"Time taken: {time_taken}")
+#     computation_time_results.append(time_taken)
+#     sparsityies.append(sparsity)
     
 
-with open("computation_time_results.csv", "w") as f:
-    for I in range(len(sparsityies)):
-        f.write(f"{sparsityies[I]},{computation_time_results[I]}\n")
+#plot
+plt.plot(computation_time_results.keys(), computation_time_results.values())
+plt.xlabel('Sparsity')
+plt.ylabel('Computation Time')
+plt.title('Computation Time vs Sparsity')
+plt.savefig('computation_time_vs_sparsity.png')
 
-plt.plot(numpy.arange(0.0, 1.01, 0.01), computation_time_results)
-plt.xlabel("Sparsity")
-plt.ylabel("Computation Time")
-plt.title("Computation Time vs Sparsity")
-plt.savefig("computation_time_vs_sparsity.png")
+#save results
+with open('computation_time_results.txt', 'w') as file:
+    file.write(str(computation_time_results))
+    file.close()
+print("Results saved")
