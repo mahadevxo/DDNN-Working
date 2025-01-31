bebe038cad05cf4c2e400d383e85cd0a200f4f9c
mahadevxo <mahadevxunilkumar03@gmail.com>
2025-01-31 18:48:40 +0530

    idek man

--- pruning/local/functions_prune.py
+++ pruning/local/functions_prune.py
@@ -10,74 +10,38 @@ import PIL
 
 class FunctionsPrune:
     
-    def __init__(self, input_size = (1, 3, 224, 224)):
-        self.input_size = input_size
-        self.model = self.get_model()
-        self.model = self.prune_model(self.model, 0.5)
-        self.model = self.convert_to_coreml(self.model, self.input_size)
-
+    def __init__(self):
+        pass
         
     def get_model(self, model_name='vgg11'):
-        """Retrieves a pretrained model from the torchvision.models library.
-
-        This function returns a specified pretrained model from torchvision.models.
-        It supports 'vgg11', 'vgg16', 'alexnet', and 'mobilenetv3'.
-
-        Args:
-            model_name (str): The name of the pretrained model to retrieve.
-
-        Returns:
-            nn.Module: The pretrained model.
-
-        Raises:
-            ValueError: If the provided model_name is not supported.
-        """
-        print('Model:', model_name)
-        
-        if model_name == 'vgg11':
-            return models.vgg11(pretrained=True)
-        elif model_name == 'vgg16':
-            return models.vgg16(pretrained=True)
-        elif model_name == 'alexnet':
-            return models.alexnet(pretrained=True)
-        elif model_name == 'mobilenetv3':
-            return models.mobilenet_v3_small(pretrained=True)
+        # print('Model:', model_name)
+        if model_name == 'MobileNetV3 Large':
+            model = models.mobilenet_v3_large(weights=models.MobileNet_V3_Large_Weights.DEFAULT)
+        elif model_name == 'AlexNet':
+            model = models.alexnet(weights=models.AlexNet_Weights.DEFAULT)
+        elif model_name == 'VGG11':
+            model = models.vgg11(weights=models.VGG11_Weights.DEFAULT)
+        elif model_name == 'VGG13':
+            model = models.vgg13(weights=models.VGG13_Weights.DEFAULT)
+        elif model_name == 'VGG16':
+            model = models.vgg16(weights=models.VGG16_Weights.DEFAULT)
+        elif model_name == 'VGG19':
+            model = models.vgg19(weights=models.VGG19_Weights.DEFAULT)
+        elif model_name == 'ConvNeXt Large':
+            model = models.convnext_large(weights=models.ConvNeXt_Large_Weights.DEFAULT)
         else:
-            raise ValueError('Unknown model name')
+            raise ValueError(f'Unsupported model: {model_name}')
+
+        return model
 
     def prune_model(self,model, prune_rate):
-        """Prunes convolutional layers in a given model using L1 unstructured pruning.
-
-        This function iterates through the model's modules and applies L1 unstructured
-        pruning to the weights of convolutional layers (nn.Conv2d) based on the
-        specified pruning rate.  It then prints the total number of parameters in the model.
-
-        Args:
-            model (nn.Module): The model to prune.
-            prune_rate (float): The pruning rate (proportion of weights to prune).
-
-        Returns:
-            nn.Module: The pruned model.
-        """
         for _, module in model.named_modules():
             if isinstance(module, nn.Conv2d):
                 prune.l1_unstructured(module, name='weight', amount=prune_rate)
         names_params = list(model.named_parameters())
-        return model, names_params
+        return model, len(names_params)
 
     def convert_to_coreml(self, model, input_shape=(1, 3, 224, 224)):
-        """Converts a PyTorch model to CoreML format.
-
-        This function traces the given PyTorch model using a random input tensor and
-        converts it to CoreML format using coremltools.
-
-        Args:
-            model (nn.Module): The PyTorch model to convert.
-            input_shape (tuple): The shape of the input tensor.
-
-        Returns:
-            coremltools.models.MLModel: The converted CoreML model.
-        """
         model.eval()
         traced_model = torch.jit.trace(model, torch.randn(input_shape))
         coremlmodel= coremltools.convert(
@@ -90,38 +54,37 @@ class FunctionsPrune:
         coremlmodel.save("coremlmodel.mlpackage")
         return coremltools.models.MLModel('coremlmodel.mlpackage')
         
-    def computation_time(self, model, use_core_ml):
+    def computation_time(self, model, use_core_ml, count):
         images = []
         if use_core_ml:
-            for _ in range(100):
+            for _ in range(count):
                 image = torch.randn(3, 224, 224).numpy().astype(numpy.float32)
                 image = image.transpose(1, 2, 0)
                 image = ((image - image.min()) / (image.max() - image.min()) * 255).astype(numpy.uint8)
                 image = PIL.Image.fromarray(image)
                 images.append({"image": image})
         else:
-            images = [torch.randn(1, 3, 224, 224) for _ in range(100)]
+            model.eval()
+            images = [torch.randn(1, 3, 224, 224) for _ in range(count)]
 
         start_time = time.time()
-        for image_dict in images:
-            if use_core_ml:
-                model.predict(image_dict)
-            else:
-                model(image_dict)
+        
+        if use_core_ml:
+            for image in images:
+                model.predict(image)
+        else:
+            with torch.no_grad():
+                for image_dict in images:
+                    _ = model(image_dict)
         end_time = time.time()
         return end_time - start_time
 
-    def plot_graph(self, computation_time, sparsities, model_name):
-        """Plots a graph of inference time vs. sparsity.
-        This function plots the given computation time against a range of
-        sparsities and saves the plot as 'inference_time_vs_sparsity.png'.
-
-        Args:
-            computation_time (list): A list of inference times.
-        """
+    def plot_graph(self, computation_time, sparsities, model_name, params):
         plt.plot(sparsities, computation_time)
         plt.xlabel('Sparsity')
         plt.ylabel('Inference Time')
-        plt.title('Inference Time vs. Sparsity')
+        plt.title(f'\n{model_name}->{params}\nInference Time vs. Sparsity')
+        plt.grid()
         plt.savefig(f'inference_time_vs_sparsity_{model_name}.png')
-        plt.show()
+        plt.clf()
+        # plt.show()
--- pruning/local/main.py
+++ pruning/local/main.py
@@ -2,35 +2,55 @@ import functions_prune
 import numpy
 import shutil
 import torch
+import os
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
 
 def get_device():
     device_available = 'cuda' if torch.cuda.is_available() else 'CoreML' if torch.backends.mps.is_available else 'CPU'
 
     return bool(
-        int(input(f'Enter 1 to use {device_available}, 0 to use PyTorch: '))
+        int(input(f'\nEnter 0 to use PyTorch on CPU or 1 to use {device_available}: '))
     )
 
 def main():
     functionsprune = functions_prune.FunctionsPrune()
-    shutil.rmtree('coremlmodel.mlpackage/')
+    if os.path.exists('coremlmodel.mlpackage/'):
+        shutil.rmtree('coremlmodel.mlpackage/')
     sparsities = numpy.arange(0, 1.05, 0.05)
-    computation_times = []
-    models = ['vgg11', 'vgg16', 'alexnet', 'mobilenetv3']
+    models = ['MobileNetV3 Large', 'AlexNet', 'VGG11', 'VGG13', 'VGG16', 'VGG19', 'ConvNeXt Large']
     coreml = get_device()
-    for model_name in models:
+    for model_using in models:
+        params = None
+        computation_times = []
+        print(f'{bcolors.BOLD}{bcolors.FAIL}Model Used: {model_using}'.center(shutil.get_terminal_size().columns))
         for sparsity in sparsities:
-            model = functionsprune.get_model(model_name)
-            model, params = functionsprune.prune_model(model, sparsity)
+            model = functionsprune.get_model(model_using)
+            model.eval()
+            model, p = functionsprune.prune_model(model, sparsity)
+            params = p if params is None else params
             if coreml:
                 model = functionsprune.convert_to_coreml(model)
                 shutil.rmtree('coremlmodel.mlpackage/')
-            inference_time = functionsprune.computation_time(model, coreml)
-            print(f'Model: {model_name}, Params: {params}, Sparsity: {sparsity}, Inference time: {inference_time:.2f} ms')
+            inference_time = functionsprune.computation_time(model, coreml, 100)
+            print(
+                f'{bcolors.WARNING}Model: {model_using}, Params: {params}, Sparsity: {sparsity:.2f}, {bcolors.OKGREEN}Inference time: {inference_time:.2f} ms'
+            )
             computation_times.append(inference_time)
 
-        functionsprune.plot_graph(computation_times, sparsities, model_name)
+        functionsprune.plot_graph(computation_times, sparsities, model_using, params)
+        params = None
 
-        with open(f'computation_times_{model_name}.csv', 'w') as f:
+        with open(f'computation_times_{model_using}.csv', 'w') as f:
             f.write('Sparsity,Inference Time\n')
             for sparsity, inference_time in zip(sparsities, computation_times):
                 f.write(f'{sparsity},{inference_time}\n')
