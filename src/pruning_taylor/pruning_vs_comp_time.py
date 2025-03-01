95f8ff13df0f733193f788d0737048d2345d868a
mahadevxo <mahadevxunilkumar03@gmail.com>
2025-03-01 15:49:10 +0530

    put model def inside loop

--- src/pruning_taylor/pruning_vs_comp_time.py
+++ src/pruning_taylor/pruning_vs_comp_time.py
@@ -5,13 +5,13 @@ import torch
 
 def main():
     device = 'mps' if torch.backends.mps.is_available() else 'cuda' if torch.cuda.is_available() else 'cpu'
-    model = models.vgg16(weights=models.VGG16_Weights.IMAGENET1K_V1).to(device)
     pruning_amounts = numpy.arange(0.0, 60.0, 1.0)
     print(f"Number of pruning percentages: {len(pruning_amounts)}")
     
     with open("pruning_vs_comp_time.csv", mode='w') as file:
         file.write("Pruning Amount, Pre-Accuracy, Accuracy, Pre-Pruning Time, Post-Pruning Time, Size\n")
         for pruning_amount in pruning_amounts:
+            model = models.vgg16(weights=models.VGG16_Weights.IMAGENET1K_V1).to(device)
             pruning_fine_tuner = PruningFineTuner(model)
             data = pruning_fine_tuner.prune(pruning_amount)
             pre = data[0]
@@ -20,6 +20,7 @@ def main():
             data = f"{pruning_amount}, {pre_acc}, {acc}, {pre_comp}, {comp_time}, {size}"
             print(data)
             file.write(data + '\n')
+            del pruning_fine_tuner
     
     print("Done")
 
