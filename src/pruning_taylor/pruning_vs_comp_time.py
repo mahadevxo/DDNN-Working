d8c90875fc0b624c3d45838a28c83cc9bf494b19
mahadevxo <mahadevxunilkumar03@gmail.com>
2025-03-01 15:04:09 +0530

    debugged testing

--- src/pruning_taylor/pruning_vs_comp_time.py
+++ src/pruning_taylor/pruning_vs_comp_time.py
@@ -6,7 +6,7 @@ import torch
 def main():
     device = 'mps' if torch.backends.mps.is_available() else 'cuda' if torch.cuda.is_available() else 'cpu'
     model = models.vgg16(weights=models.VGG16_Weights.IMAGENET1K_V1).to(device)
-    pruning_fine_tuner = PruningFineTuner(model)
+    
     
     pruning_amounts = numpy.arange(0.0, 60.0, 1.0)
     print(f"Number of pruning percentages: {len(pruning_amounts)}")
@@ -14,6 +14,7 @@ def main():
     with open("pruning_vs_comp_time.csv", mode='w') as file:
         file.write("Pruning Amount, Pre-Accuracy, Accuracy, Pre-Pruning Time, Post-Pruning Time, Size\n")
         for pruning_amount in pruning_amounts:
+            pruning_fine_tuner = PruningFineTuner(model)
             data = pruning_fine_tuner.prune(pruning_amount)
             pre = data[0]
             pre_acc, pre_comp = pre[0], pre[1]
