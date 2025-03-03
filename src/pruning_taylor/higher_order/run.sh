65955be5a97e0902d1596b7d0093f0cc9b04fb5a
mahadevxo <mahadevxunilkumar03@gmail.com>
2025-03-03 17:31:38 +0530

    final

--- src/pruning_taylor/higher_order/run.sh
+++ src/pruning_taylor/higher_order/run.sh
@@ -1,2 +1,7 @@
 echo "Running For Taylor Order 1"
-python3 
+python3 test_t_1.py
+echo "Running For Taylor Order 2"
+python3 test_t_2.py
+echo "Running For Taylor Order 3"
+python3 test_t_3.py
+echo "Done"
--- src/pruning_taylor/higher_order/test_t_3.py
+++ src/pruning_taylor/higher_order/test_t_3.py
@@ -1,24 +1,28 @@
 import torchvision.models as models
-import matplotlib.pyplot as plt
 from PruningFineTuner import PruningFineTuner
 
 def main():
-    # Instantiate a pretrained model for Taylor Order 1 pruning
     model = models.vgg16(weights=models.VGG16_Weights.IMAGENET1K_V1)
     pruning_ratios = [0, 10, 20, 30, 40, 50, 60]
-    top1_accuracies = []
 
-    with open ('results_1.csv', 'w') as f:
+    with open('results_3.csv', 'w') as f:
         f.write('Pruning Ratio, Top-1 Accuracy, Top-5 Accuracy, Model Size, Compute Time\n')
         for ratio in pruning_ratios:
             tuner = PruningFineTuner(model)
-            results = tuner.prune(ratio)  # results: [pre_fine_tuning_acc, raw_acc, compute_time, size_mb]
-            top1_accuracies.append(results[1])
+            def get_candidates_to_prune_3nd(num_filter_to_prune):
+                tuner.pruner.reset()
+                tuner.train_epoch(rank_filter=True)
+                tuner.pruner.filter_ranks = tuner.pruner.filter_ranks_3rd
+                tuner.pruner.normalize_ranks_per_layer()
+                return tuner.pruner.get_pruning_plan(num_filter_to_prune)
+            tuner.get_candidates_to_prune = get_candidates_to_prune_3nd
+
+            results = tuner.prune(ratio) 
+            # results: [pre_fine_tuning_acc, acc 1%, 5%, compute_time, model_size]
             data = f"Pruning {ratio}%: Top-1 Accuracy = {results[1]*100:.2f}%, Top-5 Accuracy = {results[2]*100:.2f}%, Model Size = {results[4]:.2f}MB, Compute Time = {results[3]:.2f}s"
             print(data)
             f.write(f"{ratio}, {data}\n")
-            # Reload the model for the next iteration
             model = models.vgg16(weights=models.VGG16_Weights.IMAGENET1K_V1)
-        
+
 if __name__ == '__main__':
     main()
