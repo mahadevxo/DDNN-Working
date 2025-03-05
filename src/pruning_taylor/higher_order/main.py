98cd070a96f307e59f8e9a9fff5dbd1082452ea0
mahadevxo <mahadevxunilkumar03@gmail.com>
2025-03-05 13:47:34 +0530

    cleaned it upo

---
+++ src/pruning_taylor/higher_order/main.py
@@ -0,0 +1,20 @@
+import torchvision.models as models
+from SearchAlgorithm import SearchAlgorithm
+from PruningFineTuner import PruningFineTuner
+import torch
+def main():
+    device = 'mps' if torch.backends.mps.is_available() else 'cuda' if torch.cuda.is_available() else 'cpu'
+    model = models.vgg16(weights=models.VGG16_Weights.IMAGENET1K_V1).to(device)
+    pruning_fine_tuner = PruningFineTuner(model)
+    out = pruning_fine_tuner.test(model)
+    print(f"Original Accuracy: {out[0]*100:.2f}% (Top-1), {out[3]*100:.2f}% (Top-5)")
+    print(f'Original Model Size: {pruning_fine_tuner.get_model_size(model):.2f} MB')
+    del pruning_fine_tuner
+    min_acc = float(input("Enter minimum acceptable accuracy (0-100): "))
+    print(f"Minimum Accuracy: {min_acc}%")
+    searching_strategy = SearchAlgorithm(model, min_accuracy=(min_acc/100))
+    best_percentage = searching_strategy.heuristic_binary_search()
+    print(f"Recommended pruning percentage: {best_percentage:.2f}%")
+
+if __name__ == '__main__':
+    main()
---
+++ src/pruning_taylor/higher_order/test_t_2.py
@@ -0,0 +1,43 @@
+import torchvision.models as models
+from PruningFineTuner import PruningFineTuner
+import numpy as np
+
+def main():
+    model = models.vgg16(weights=models.VGG16_Weights.IMAGENET1K_V1)
+    # try:
+    #     pruning_ratios = [int(input("Enter the pruning ratio: "))]
+    #     print(pruning_ratios)
+    # except Exception as e:
+    #     pruning_ratios = None
+    #     print(f"Error: {str(e)}")
+    #     print("No pruning ratio entered. Using default values.")
+    # finally:
+    #     if pruning_ratios is None:
+    #         pruning_ratios = np.arange(0, 100, 10).tolist()
+    
+    pruning_ratios = np.arange(0, 100, 10).tolist()
+    
+    with open('results_2.csv', 'w') as f:
+        f.write('Pruning Ratio, Accuracy, Compute Time, Model Size\n')
+        for ratio in pruning_ratios:
+            try:
+                tuner = PruningFineTuner(model, 2) # Taylor Order 2
+                def get_candidates_to_prune_2nd(num_filter_to_prune):
+                    tuner.pruner.reset()
+                    tuner.train_epoch(rank_filter=True)
+                    tuner.pruner.filter_ranks = tuner.pruner.filter_ranks_2nd
+                    tuner.pruner.normalize_ranks_per_layer()
+                    return tuner.pruner.get_pruning_plan(num_filter_to_prune)
+                tuner.get_candidates_to_prune = get_candidates_to_prune_2nd
+                results = tuner.prune(ratio) 
+                data = f"{ratio, results[1], results[2], results[3]}"
+                print(data)
+                f.write(f"{data}\n")
+            except Exception as e:
+                f.write(f"{ratio}, -1, -1, -1\n")
+                print(f"Error: {str(e)}")
+                continue
+            model = models.vgg16(weights=models.VGG16_Weights.IMAGENET1K_V1)
+
+if __name__ == '__main__':
+    main()
