72455f71709d9c43a1b7005776fb5fed96874874
mahadevxo <mahadevxunilkumar03@gmail.com>
2025-03-13 09:58:47 +0530

    fixed error

--- src/pruning_taylor/main.py
+++ src/pruning_taylor/main.py
@@ -7,7 +7,7 @@ def main():
     model = models.vgg16(weights=models.VGG16_Weights.IMAGENET1K_V1).to(device)
     pruning_fine_tuner = PruningFineTuner(model)
     out = pruning_fine_tuner.test(model)
-    print(f"Original Accuracy: {out[0]*100:.2f}%")
+    print(f"Original Accuracy: {out[0]:.2f}%")
     print(f'Original Model Size: {pruning_fine_tuner.get_model_size(model):.2f} MB')
     del pruning_fine_tuner
     min_acc = float(input("Enter minimum acceptable accuracy (0-100): "))
