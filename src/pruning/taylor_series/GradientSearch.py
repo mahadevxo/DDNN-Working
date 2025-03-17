e7ff0f6fb7fa257420b9c31052e11f51c1349fdf
mahadevxo <mahadevsunilkumar03@gmail.com>
2025-03-17 15:09:33 +0530

    added reward function

---
+++ src/pruning/taylor_series/GradientSearch.py
@@ -0,0 +1,3 @@
+class GradientSearch:
+    def __init__(self):
+        pass
---
+++ src/pruning/taylor_series/RewardFuntion.py
@@ -0,0 +1,44 @@
+class RewardFuntion:
+    def __init__(self, INITIAL_ACC: float, ACC_IMP: float, 
+                 COMP_IMP: float, MODEL_SIZE_IMP: float):
+        self.X = ACC_IMP
+        self.Y = COMP_IMP
+        self.Z = MODEL_SIZE_IMP
+        self.accuracy_min = INITIAL_ACC
+        
+        if self.X + self.Y + self.Z != 1:
+            raise ValueError("Sum of all weights should be 1")
+    
+    def _accuracy_reward(self, accuracy_new) -> float:
+        
+        if accuracy_new > (self.accuracy_min - 10**-3) and accuracy_new < (self.accuracy_min + 10**-3):
+            return 10000.0
+        elif accuracy_new > (self.accuracy_min + 10**-3):
+            return (accuracy_new - self.accuracy_min)*100
+        else:
+            return (self.accuracy_min - accuracy_new)*-10000.0
+    
+    def _model_size_reward(self, model_size_new, model_size_previous) -> float:
+        
+        delta_model_size = model_size_new - model_size_previous
+        if delta_model_size < 0:
+            return 1000 * delta_model_size
+        else:
+            return 3000 * delta_model_size
+    
+    def _comp_time_reward(self, comp_time_new, comp_time_previous) -> float:
+        
+        delta_comp_time = comp_time_new - comp_time_previous
+        if delta_comp_time < 0:
+            return 1000 * delta_comp_time
+        else:
+            return 3000 * delta_comp_time
+    
+    def get_reward(self, acc_new: float, model_size_new: float, model_size_previous: float, 
+                   comp_time_new: float, comp_time_previous: float) -> float:
+        
+        acc_reward = self._accuracy_reward(acc_new)
+        model_size_reward = self._model_size_reward(model_size_new, model_size_previous)
+        comp_time_reward = self._comp_time_reward(comp_time_new, comp_time_previous)
+        
+        return self.X*acc_reward + self.Y*model_size_reward + self.Z*comp_time_reward
