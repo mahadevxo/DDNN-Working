4713236fbc70fe30ecc1f6c93df6b9ccb7152703
mahadevxo <mahadevsunilkumar03@gmail.com>
2025-03-18 00:14:44 +0530

    added exception

--- src/pruning/taylor_series/test.py
+++ src/pruning/taylor_series/test.py
@@ -7,22 +7,25 @@ def main():
     pruning_amounts = np.arange(0, 60, 0.25)
     device = 'mps' if torch.backends.mps.is_available() else 'cuda' if torch.cuda.is_available() else 'cpu'
     text = "Pruning Amount, Final Accuracy, Time, Memory\n"
-    for pruning_amount in pruning_amounts:
-        model = models.vgg16(weights=models.VGG16_Weights.IMAGENET1K_V1).to(device)
-        pruning_fine_tuner = PruningFineTuner(model)
-        pruning_fine_tuner.prune(pruning_percentage=pruning_amount)
-        out = pruning_fine_tuner.test(model)
-        model_size = pruning_fine_tuner.get_model_size(model)
-        text += f"{pruning_amount:.2f}%, {out[0]}%,{out[1]:.2f}, {model_size}\n"
-        print(f"Pruning Amount: {pruning_amount:.2f}%, Final Accuracy: {out[0]:.2f}%, Time: {out[1]:.2f}, Memory: {model_size:.2f}")
-        del pruning_fine_tuner
-        model.cpu()
-        del model
-        torch.cuda.empty_cache()
-    
-    with open('pruning_results.csv', 'w') as f:
-        f.write(text)
-    print("Results saved to pruning_results.csv")
+    try:
+        for pruning_amount in pruning_amounts:
+            model = models.vgg16(weights=models.VGG16_Weights.IMAGENET1K_V1).to(device)
+            pruning_fine_tuner = PruningFineTuner(model)
+            pruning_fine_tuner.prune(pruning_percentage=pruning_amount)
+            out = pruning_fine_tuner.test(model)
+            model_size = pruning_fine_tuner.get_model_size(model)
+            text += f"{pruning_amount:.2f}%, {out[0]}%,{out[1]:.2f}, {model_size}\n"
+            print(f"Pruning Amount: {pruning_amount:.2f}%, Final Accuracy: {out[0]:.2f}%, Time: {out[1]:.2f}, Memory: {model_size:.2f}")
+            del pruning_fine_tuner
+            model.cpu()
+            del model
+            torch.cuda.empty_cache()
+    except Exception as e:
+        print(e)   
+        with open('pruning_results.csv', 'w') as f:
+            f.write(text)
+        print("Results saved to pruning_results.csv")
+        exit()
         
 if __name__ == '__main__':
     main()
