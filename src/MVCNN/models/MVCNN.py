32aae039b892c28956ed906dfc1ef1581796f173
mahadevxo <mahadevsunilkumar03@gmail.com>
2025-03-28 14:05:12 +0530

    updated train

--- src/MVCNN/models/MVCNN.py
+++ src/MVCNN/models/MVCNN.py
@@ -97,6 +97,6 @@ class MVCNN(Model):
 
     def forward(self, x):
         y = self.net_1(x)
-        y = y.view((int(x.shape[0]/self.num_views),self.num_views,y.shape[-3],y.shape[-2],y.shape[-1]))#(8,12,512,7,7)
-        return self.net_2(torch.max(y,1)[0].view(y.shape[0],-1))
+        y = y.view(y.size(0), -1)
+        return self.net_2(y)
 
