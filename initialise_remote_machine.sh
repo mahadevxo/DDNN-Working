529d18a5bfc71e99b992baa5de22c6efce59fdf0
mahadevxo <mahadevsunilkumar03@gmail.com>
2025-06-20 11:32:43 +0530

    added filter counting

--- initialise_remote_machine.sh
+++ initialise_remote_machine.sh
@@ -3,6 +3,6 @@ bash -e -c 'sudo apt update ; sudo apt install -y vim unzip ; wget https://repo.
 
 bash -e -c 'conda create -n ML python==3.10 ; conda activate ML ; pip install numpy torch torchvision matplotlib pandas tensorboardx tqdm scikit-image scikit-learn cma-es ; git clone "https://github.com/mahadevxo/DDNN-Working.git" ; sudo apt install -y nvtop ; conda deactivate ; pip install nvitop ; conda activate ML ; pip install nvitop'
 
-bash -e -c 'cd DDNN-Working/src/test-models/ ; curl -L -o ./imagenetmini-1000.zip https://www.kaggle.com/api/v1/datasets/download/ifigotin/imagenetmini-1000 ; sudo apt install -y unzip ; unzip imagenetmini-1000.zip'
+bash -e -c 'cd DDNN-Working/src/pruning/taylor_series/better-pruning ; curl -L -o ./imagenetmini-1000.zip https://www.kaggle.com/api/v1/datasets/download/ifigotin/imagenetmini-1000 ; sudo apt install -y unzip ; unzip imagenetmini-1000.zip'
 
 bash -e -c 'cd ~/DDNN-Working/src/MVCNN ; curl -L -o ./ModelNet40.zip http://modelnet.cs.princeton.edu/ModelNet40.zip ; unzip ModelNet40.zip ; rm ModelNet40.zip ; python3 multi_convert.py'
