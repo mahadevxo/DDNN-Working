5b234167fef52ee87cc948bdf69857271286fc9e
mahadevxo <mahadevsunilkumar03@gmail.com>
2025-07-11 12:33:35 +0530

    updated path

--- src/pruning/taylor_series/better-pruning/main_mvcnn.py
+++ src/pruning/taylor_series/better-pruning/main_mvcnn.py
@@ -139,7 +139,7 @@ def get_model() -> torch.nn.Module:
         nclasses=33,
         cnn_name="vgg11"
     )
-    weights = torch.load("../../../MVCNN/MVCNN/model-00050.pth", map_location=device)
+    weights = torch.load("../../../MVCNN/MVCNN/model-model-00050.pth", map_location=device)
     model.load_state_dict(weights, strict=False)
     model = model.to(device)
     return model
