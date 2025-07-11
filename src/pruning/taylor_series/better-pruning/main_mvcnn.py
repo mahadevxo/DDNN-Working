dc2dd640b582ef8f72ac3271e24ab053b1f48218
mahadevxo <mahadevsunilkumar03@gmail.com>
2025-07-11 17:54:53 +0530

    changed pruning amounts

--- src/pruning/taylor_series/better-pruning/main_mvcnn.py
+++ src/pruning/taylor_series/better-pruning/main_mvcnn.py
@@ -187,7 +187,7 @@ def get_model() -> torch.nn.Module:
 def main() -> None:
     # Define pruning range - EXTREMELY AGGRESSIVE
     pruning_amounts = np.array([
-        0.0, 0.3, 0.5, 0.7, 0.8, 0.9, 0.95, 0.97, 0.98, 0.99
+        0.10, 0.20, 0.40, 0.60, 0.05, 0.01, 0.15, 0.25, 0.65, 0.45, 0.85
     ])
     pruning_amounts = list(np.random.permutation(pruning_amounts))
     #randomize it
