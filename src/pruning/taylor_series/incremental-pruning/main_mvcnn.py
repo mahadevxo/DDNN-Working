a9b1d19b9a1e08dab0003a39aa2cbbb844c895e4
mahadevxo <mahadevsunilkumar03@gmail.com>
2025-08-03 02:11:45 +0530

    save models

--- src/pruning/taylor_series/incremental-pruning/main_mvcnn.py
+++ src/pruning/taylor_series/incremental-pruning/main_mvcnn.py
@@ -174,9 +174,9 @@ def get_model() -> torch.nn.Module:
 
 def main() -> None:
     
-    # save_models = True; pruning_amounts = [0.0, 0.1, 0.3, 0.5, 0.9]  # noqa: E702
-    save_models=False; pruning_amounts = np.arange(0.0, 1, 0.05).tolist()  # noqa: E702
-    # pruning_amounts = list(np.random.permutation(pruning_amounts))
+    save_models = True; pruning_amounts = [0.0, 0.1, 0.3, 0.5, 0.9]  # noqa: E702
+    # save_models=False; pruning_amounts = np.arange(0.0, 1, 0.05).tolist()  # noqa: E702
+    
     
     for part_my_part_prune in [False]:
         current_time_str = time.strftime("%Y%m%d-%H%M%S")
